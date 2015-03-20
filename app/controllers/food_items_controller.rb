39dce4e8ebab2e9e1c45df5f5ab012c8d6186899
rodic <alec.rodic@gmail.com>
2015-03-20 20:01:41 +0100

    Removed #keyword_capitalized from FoodItem

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -4,7 +4,7 @@ class FoodItemsController < ApplicationController
     @food_hash = Hash.new([])
 
     FoodItem.order(:keyword).each do |f|
-      @food_hash[f.keyword_capitalized] += [f]
+      @food_hash[f.keyword.mb_chars.capitalize.to_s] += [f]
     end
   end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -15,15 +15,4 @@ class FoodItem < ActiveRecord::Base
   def downcase_keyword
     self.keyword.downcase!
   end
-
-  def keyword_capitalized
-    keyword = self.keyword.capitalize
-    keyword[0] = "Š" if keyword[0] == "š"
-    keyword[0] = "Đ" if keyword[0] == "đ"
-    keyword[0] = "Č" if keyword[0] == "č"
-    keyword[0] = "Ć" if keyword[0] == "ć"
-    keyword[0] = "Ž" if keyword[0] == "ž"
-    keyword
-  end
-
 end
